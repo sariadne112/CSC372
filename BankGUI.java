644af263647e4fc1715c944858108ddf0322f58a
Ariadne Nava <ariadneg112@gmail.com>
2026-08-29 23:33:38 -0400

    added sign in button

--- BankGUI.java
+++ BankGUI.java
@@ -6,23 +6,21 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.security.spec.EdDSAParameterSpec;
-
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class BankGUI extends BankAccount{
+public class BankGUI extends BankAccount implements ActionListener{
+    private static BankAccount account;
+    private static JTextField accountField;  // Displays account field
+    private static JButton enterButton; //Enters account
   public static void main(String[] args) {
-      
       JFrame topFrame = null;                // Application window
-      JLabel accountLabel = null;               // Label for user account
-      JTextField accountField = null;            // Displays hourly salary 
+      JLabel accountLabel = null;               // Label for user accoun
       GridBagConstraints layoutConst = null; // GUI component layout
-      
+      account = new BankAccount();
       
       //Set Balance Field
       accountLabel = new JLabel("Account:");
-  
       accountField = new JTextField(20);
       accountField.setEditable(true);
 
@@ -33,14 +31,13 @@ public class BankGUI extends BankAccount{
       topFrame.setLayout(new GridBagLayout());
       
       topFrame.setSize(500, 350);
-
+      //create Enter Button
+      enterButton = new JButton("Sign In");
       // Create GridBagConstraints
       layoutConst = new GridBagConstraints();
     
-      
       // 10 pixels of padding around component
       layoutConst.insets = new Insets(10, 10, 10, 10);
-      
 
       layoutConst.gridx = 0;
       layoutConst.gridy = 0;
@@ -49,9 +46,17 @@ public class BankGUI extends BankAccount{
       layoutConst.gridx = 1;
       layoutConst.gridy = 0;
       topFrame.add(accountField, layoutConst);
-      // Terminate program when window closes
+
+      layoutConst.gridx = 0;
+      layoutConst.gridy = 2;
+      layoutConst.insets = new Insets(10, 10, 10, 10);
+      topFrame.add(enterButton, layoutConst);
+
+      //account value
+      accountField.setText(String.valueOf(account.getAccountID()));
+       
+       // Terminate program when window closes
       topFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      
       
       // Resize window to fit components
       topFrame.pack();
@@ -59,4 +64,10 @@ public class BankGUI extends BankAccount{
       // Display window
       topFrame.setVisible(true);
    }
+
+    public void actionPerformed(ActionEvent event) {
+    int accountID = Integer.parseInt(accountField.getText());
+    account.setAccountID(accountID);
+
+    System.out.println("Account ID entered: " + account.getAccountID());}
 }
