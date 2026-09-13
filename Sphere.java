5677d67cc06f29a530ad4850f2544ab3e805b81f
Ariadne Nava <ariadneg112@gmail.com>
2026-09-13 01:58:22 -0400

    Sphere fixed synat error

--- Sphere.java
+++ Sphere.java
@@ -1,17 +1,23 @@
-public class Sphere extends Shapes{
+public class Sphere extends Shape{
   private double radius;
-
+   //radius constructor
   public Sphere(double radius) {
       this.radius = radius;
    }
-
+   //surface area
    @Override
    public double surface_area(){
     return 4.0 * Math.PI * Math.pow(radius, 2);
     }
-
-    @Override
+  //volume
+  @Override
     public double volume() {
-        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+      return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+    }
+  //To String
+  @Override
+    public String toString() {
+      return String.format("Sphere Surface Area: %.2f%nSphere Volume: %.2f",
+        surface_area(), volume());
     }
 }
