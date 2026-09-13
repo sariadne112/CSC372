7f058ec9419cdf96b24f7831a79bd3c20c6012e8
Ariadne Nava <ariadneg112@gmail.com>
2026-09-13 01:57:17 -0400

    Added Instantiate

--- ShapeArray.java
+++ ShapeArray.java
@@ -1,3 +1,17 @@
-public class ShapeArray extends Shapes{
-  
+public class ShapeArray{
+  public static void main(String[] args) {
+    //Instantiate Sphere
+    Sphere sphere = new Sphere(50.0);
+    //Instantiate Cylinder
+    Cylinder cylinder = new Cylinder(50.0, 20.00);
+    //Instantiate Cone
+    Cone cone = new Cone(50.00, 25.00);
+    //Store Array
+    Shape[] shapeArray = {sphere, cylinder, cone};
+    //loop through
+    for(Shape shape : shapeArray) {
+      System.out.println(shape.toString());
+      System.out.println();
+    }
+  }
 }
