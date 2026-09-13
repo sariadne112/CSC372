5d9ae726cff87855eafec46fdbf2ae79c85d5cce
Ariadne Nava <ariadneg112@gmail.com>
2026-09-13 00:53:35 -0400

    added cylinder

--- Cylinder.java
+++ Cylinder.java
@@ -1,3 +1,26 @@
 public class Cylinder {
-  
+  private double radius;
+  private double height;
+
+  public Cylinder(double radius, double height){
+    this.radius = radius;
+    this.height = height;
+  }
+  //surface area
+  @Override
+  public double surface_area() {
+    return (2 * Math.PI * radius * height)
+    + (2 * Math.PI * Math.pow(radius, 2));
+    }
+
+  public double volume(){
+    return Math.PI * Math.pow(radius, 2) * height;
+  }
+
+  //To String
+  @Override
+    public String toString() {
+        String.format("Sphere Surface Area: %.2f%nSphere Volume: %.2f",
+        surface_area(), volume());
+    }
 }
