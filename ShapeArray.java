f8e9cee45f47b1ccfe54cfb99f9b6eb13b9d1928
Ariadne Nava <ariadneg112@gmail.com>
2026-09-13 03:39:08 -0400

    changed volume and radius

--- ShapeArray.java
+++ ShapeArray.java
@@ -3,9 +3,9 @@ public class ShapeArray{
     //Instantiate Sphere
     Sphere sphere = new Sphere(9.0);
     //Instantiate Cylinder
-    Cylinder cylinder = new Cylinder(7, 15.67);
+    Cylinder cylinder = new Cylinder(10, 15.97);
     //Instantiate Cone
-    Cone cone = new Cone(8.9, 12.00);
+    Cone cone = new Cone(4, 8.00);
     //Store Array
     Shape[] shapeArray = {sphere, cylinder, cone};
     //loop through
--- Shapes.java
+++
@@ -1,7 +0,0 @@
-public abstract class Shapes {
-  protected Point position;
-
-   abstract double surface_area();
-   abstract double volume();
-
-}
